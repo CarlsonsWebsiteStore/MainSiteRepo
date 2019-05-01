1ce24a82ec700f3ee3e9cc9f2fbbd057fb26821e
Jordan Hill <33611224+coderguy1777@users.noreply.github.com>
2019-04-30 22:55:10 -0400

    Add files via upload

---
+++ ColorPicker.js
@@ -0,0 +1,10 @@
+
+let imgcont = document.getElementById("imageContainerTest");
+
+function showContainer() {
+    console.log("Hello");
+    let imgcont = document.getElementById("imageContainerTest");
+    imgcont.style.width = 100 + "px";
+    imgcont.style.backgroundColor = "white";
+    imgcont.style.height = 100 + "px";
+}
